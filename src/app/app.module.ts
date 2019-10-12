0706451adaa4d31ac07c7520a869c204f6660d94
Ajay Sreedhar <44643319+ajaysree2510@users.noreply.github.com>
2019-10-12 09:23:35 -0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,21 +8,23 @@ import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
-import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const appRoutes:Routes =[
-    {path:'home', component:HomeComponent},
-    {path:'',redirectTo:'home', pathMatch:'full'},
-    {path:'about', component:AboutComponent},
-    {path:'blog/:blogId', component:BlogViewComponent},
-    {path:'create', component:BlogCreateComponent},
-    {path:'edit/:blogId', component:BlogEditComponent},
-    {path:'**', component:NotFoundComponent},
-]
+
+        {path:'home', component:HomeComponent},
+        {path:'',redirectTo:'home', pathMatch:'full'},
+        {path:'blog/:blogId', component:BlogViewComponent},
+        {path:'about', component:AboutComponent},
+        {path:'edit/:blogId', component:BlogEditComponent},
+        {path:'create', component:BlogCreateComponent},
+        {path:'**', component:NotFoundComponent},
+        
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-blog-view',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogViewComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _route:ActivatedRoute, private router:Router) { 
+    console.log("constructor is called");
+  }
+  
   ngOnInit() {
+
+    console.log("ngOnInit is callled");
+    
+
   }
 
 }
